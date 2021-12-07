5ba04abdbabb5855a96e426fce6ca4d86e31b895
Vasystus <artgalla8@gmail.com>
2021-12-07 00:08:09 +0000

    Slideshow section added adn styled!

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ function changeBackground() {
     element.classList.toggle("dark-mode");
   }
 
-  function playAudio(url) {
-    new Audio(url).play();
-  }
+  
+// Slideshow
+const slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+const slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+let index = 0, time = 4000;
+	slides[index].classList.add('active');
+
+	setInterval( () => {
+		slides[index].classList.remove('active');
+		
+		index++;
+		if (index === slides.length) index = 0;
+
+		slides[index].classList.add('active');
+
+	}, time);
+}
